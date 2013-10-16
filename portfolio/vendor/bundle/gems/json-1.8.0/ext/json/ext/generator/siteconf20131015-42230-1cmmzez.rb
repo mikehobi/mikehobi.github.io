6adcb1f4b87ee7d9ea7364b476ffb38d95d498f4
mikehobi <mikehobizal@gmail.com>
2013-10-15 18:58:20 -0700

    added a lot of shit

---
+++ portfolio/vendor/bundle/gems/json-1.8.0/ext/json/ext/generator/siteconf20131015-42230-1cmmzez.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20131015-42230-1ygv813"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
---
+++ portfolio/vendor/bundle/gems/sqlite3-1.3.8/ext/sqlite3/siteconf20131015-42230-8ukccb.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20131015-42230-phm1nb"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
