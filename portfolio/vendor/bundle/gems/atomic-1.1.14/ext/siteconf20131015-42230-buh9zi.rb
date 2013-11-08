36ce02b2fc30135681aa227120ab7d5b4e16fa79
mikehobi <mikehobizal@gmail.com>
2013-11-07 16:03:30 -0800

    overhaul

--- portfolio/vendor/bundle/gems/atomic-1.1.14/ext/siteconf20131015-42230-buh9zi.rb
+++
@@ -1,6 +0,0 @@
-require 'rbconfig'
-dest_path = "./.gem.20131015-42230-1lpss50"
-RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
-RbConfig::CONFIG['sitearchdir'] = dest_path
-RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
-RbConfig::CONFIG['sitelibdir'] = dest_path
